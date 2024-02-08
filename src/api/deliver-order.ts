f83850fa265e1bf575cb926c33794c0b113ac061
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2024-02-08 15:17:17 -0300

    Test E2E - playwright

--- src/api/deliver-order.ts
+++ src/api/deliver-order.ts
@@ -1,9 +1,9 @@
 import { api } from '@/lib/axios'
 
-export interface deliverOrderParams {
+export interface DeliverOrderParams {
   orderId: string
 }
 
-export async function deliverOrder({ orderId }: deliverOrderParams) {
+export async function deliverOrder({ orderId }: DeliverOrderParams) {
   await api.patch(`/orders/${orderId}/deliver`)
 }
